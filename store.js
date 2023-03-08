efeb2af03f764794ed57c1d28430f3dd6e4ef1a3
Alexander Candia <tony_john_08@hotmail.com>
2023-03-08 18:18:47 -0500

    finished project

---
+++ store.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById('btn');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
+
+
